a50b1d7705b55bb889241e8e18c3508c5e1a2a85
DESKTOP-U3T4CJS\rest1312 <kittan.koba.1312@gmail.com>
2023-11-06 01:41:06 +0900

    初期データの登録作業・進捗追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,5 +13,20 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
+        DB::table('users')->insert([
+            [
+                'over_name' => '佐藤',
+                'under_name' => '一郎',
+                'over_name_kana' => 'サトウ',
+                'under_name_kana' => 'イチロウ',
+                'mail_address' => 'hogehoge@fmail.com',
+                'sex' => '1',
+                'birth_day' => '1989-03-10',
+                'role' => '4',
+                'password' => 'sato3100',
+                'remember_token' => ''
+            ]
+        ]);
+
     }
 }
